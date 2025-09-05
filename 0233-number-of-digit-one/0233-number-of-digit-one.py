c7335a7983bc33c2dbdcb47a1ccd8c796e8c5efb
AdithyaRajagopalan24 <adithyarajagopalancollege24@gmail.com>
2025-09-05 16:20:23 +0530

    Time: 0 ms (100%), Space: 17.6 MB (99.1%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        nextDigit = 10
+        currentDigit = 1
+        totalOnes = 0
+
+        while n >= currentDigit:
+            div = n // nextDigit
+            mod = n % nextDigit
+            totalOnes += div * currentDigit
+            if mod >= currentDigit:
+                totalOnes += min(mod, currentDigit * 2 - 1) - (currentDigit - 1)
+            nextDigit *= 10
+            currentDigit *= 10
+        return totalOnes
