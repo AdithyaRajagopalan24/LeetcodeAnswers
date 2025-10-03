fdff55eef4230bf905af0df06b0c5ed6154c9d93
AdithyaRajagopalan24 <adithyarajagopalancollege24@gmail.com>
2025-10-03 14:45:32 +0530

    Time: 218 ms (10.69%), Space: 21.5 MB (28.09%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -1,35 +1,47 @@
-from heapq import heappush, heappop
-from typing import List
+import heapq
+
+class Cell:
+    def __init__(self, height, row, col):
+        self.height = height
+        self.row = row
+        self.col = col
+
+    def __lt__(self, other):
+        return self.height < other.height
 
 class Solution:
-    def trapRainWater(self, heightMap: List[List[int]]) -> int:
-        if not heightMap or not heightMap[0]:
-            return 0
-        
-        m, n = len(heightMap), len(heightMap[0])
-        visited = [[False] * n for _ in range(m)]
-        heap = []
-        
-        # Add all boundary cells to the heap
-        for i in range(m):
-            for j in [0, n - 1]:
-                heappush(heap, (heightMap[i][j], i, j))
-                visited[i][j] = True
-        
-        for j in range(n):
-            for i in [0, m - 1]:
-                heappush(heap, (heightMap[i][j], i, j))
-                visited[i][j] = True
-        
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        trapped_water = 0
-        while heap:
-            height, x, y = heappop(heap)
+    def check(self, x, y, n, m):
+        return 0 <= x < n and 0 <= y < m
+
+    def trapRainWater(self, heightMap):
+        n = len(heightMap)
+        m = len(heightMap[0])
+        trappedWater = 0
+        priorityQueue = []
+        visited = [[False] * m for _ in range(n)]
+
+        for i in range(n):
+            heapq.heappush(priorityQueue, Cell(heightMap[i][0], i, 0))
+            heapq.heappush(priorityQueue, Cell(heightMap[i][m - 1], i, m - 1))
+            visited[i][0] = visited[i][m - 1] = True
+
+        for j in range(m):
+            heapq.heappush(priorityQueue, Cell(heightMap[0][j], 0, j))
+            heapq.heappush(priorityQueue, Cell(heightMap[n - 1][j], n - 1, j))
+            visited[0][j] = visited[n - 1][j] = True
+
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+        while priorityQueue:
+            cell = heapq.heappop(priorityQueue)
             for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
-                    trapped_water += max(0, height - heightMap[nx][ny])
-                    heappush(heap, (max(height, heightMap[nx][ny]), nx, ny))
-                    visited[nx][ny] = True
-        
-        return trapped_water
+                x, y = cell.row + dx, cell.col + dy
+                if self.check(x, y, n, m) and not visited[x][y]:
+                    if heightMap[x][y] < cell.height:
+                        trappedWater += cell.height - heightMap[x][y]
+                        heapq.heappush(priorityQueue, Cell(cell.height, x, y))
+                    else:
+                        heapq.heappush(priorityQueue, Cell(heightMap[x][y], x, y))
+                    visited[x][y] = True
+
+        return trappedWater
