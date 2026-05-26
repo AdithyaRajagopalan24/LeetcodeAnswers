0e20edbb0b0b0a00055cff3b2bc97b473b0ea414
AdithyaRajagopalan24 <adithyarajagopalancollege24@gmail.com>
2026-05-26 13:11:03 +0530

    Time: 10 ms (23.84%), Space: 20.5 MB (45.35%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,24 +4,13 @@ class Solution:
         parent = list(range(n))
 
         def find(x):
-            while parent[x] != x:
-                x = parent[x]
-            return x
-
-        def union(a, b):
-            pa = find(a)
-            pb = find(b)
-            if pa != pb:
-                parent[pb] = pa
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
 
         for i in range(n):
-            for j in range(i + 1, n):
+            for j in range(n):
                 if isConnected[i][j]:
-                    union(i, j)
+                    parent[find(i)] = find(j)
 
-        provinces = 0
-        for i in range(n):
-            if find(i) == i:
-                provinces += 1
-
-        return provinces
+        return sum(find(i) == i for i in range(n))
