4556e1768f892058bc02c4104e61ee27534ab0f3
AdithyaRajagopalan24 <adithyarajagopalancollege24@gmail.com>
2026-06-29 19:58:46 +0530

    Time: 864 ms (5.09%), Space: 0B (100%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,1 @@
+SELECT request_at AS Day, ROUND(AVG(status <> 'completed'), 2) AS 'Cancellation Rate' FROM Trips WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03' AND client_id IN (SELECT users_id FROM Users WHERE banned = 'No') AND driver_id IN (SELECT users_id FROM Users WHERE banned = 'No') GROUP BY request_at;
